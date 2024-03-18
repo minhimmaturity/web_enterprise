3197018707c81791121c3af4eb0d98ab66afb741
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-03-18 16:46:28 +0700

    enable cors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
 const auth = require("./route/user/auth.route");
-const admin = require("./route/user/admin.route")
-const user  = require("./route/user/user.route")
+const admin = require("./route/user/admin.route");
+const user = require("./route/user/user.route");
 const bodyParser = require("body-parser");
-const hbs = require('hbs');
-
+const hbs = require("hbs");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -17,17 +17,20 @@ const prisma = new PrismaClient();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'hbs');
-hbs.registerHelper('helper_name', function (options) { return 'helper value'; });
-hbs.registerPartial('partial_name', 'partial value');
+app.use(cors());
+app.set("view engine", "hbs");
+hbs.registerHelper("helper_name", function (options) {
+  return "helper value";
+});
+hbs.registerPartial("partial_name", "partial value");
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
 const main = async () => {
   try {
-    app.use("/user", user)
-    app.use("/auth", auth)
+    app.use("/user", user);
+    app.use("/auth", auth);
     app.use("/admin", admin);
     app.listen(PORT, () => {
       console.log(`Server is starting at http://${HOST}:${PORT}`);
