6c93c70c3c8f0c3d5137447d1227a3adc56b81d6
minhimmaturity <trantanminh0603@gmail.com>
2024-03-31 08:40:00 +0700

    feat chat-real-time

--- src/index.js
+++ src/index.js
@@ -5,23 +5,21 @@ const { PrismaClient } = require("@prisma/client");
 const auth = require("./route/user/auth.route");
 const admin = require("./route/user/admin.route");
 const user = require("./route/user/user.route");
+const chat = require("./route/chat/chat.route");
 const manager = require("./route/user/manager.route");
 const coordinator = require("./route/user/coordinator.route");
 const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const cors = require("cors");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const WebSocket = require('ws');
+const { join } = require('path');
 
 dotenv.config();
 
 const app = express();
 app.use(morgan("combined"));
 const prisma = new PrismaClient();
-const httpServer = createServer(app); // Attach Express app to HTTP server
-const io = new Server(httpServer, {
-  /* options */
-});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -38,6 +36,7 @@ app.use(
     ],
   })
 );
+
 app.set("view engine", "hbs");
 hbs.registerHelper("helper_name", function (options) {
   return "helper value";
@@ -45,27 +44,29 @@ hbs.registerHelper("helper_name", function (options) {
 
 hbs.registerPartial("partial_name", "partial value");
 
-const HOST = process.env.HOST;
-const PORT = process.env.PORT;
+const server = app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`Server is starting at http://${process.env.HOST}:${process.env.PORT}`);
+});
 
-const main = async () => {
-  try {
-    app.use("/user", user);
-    app.use("/auth", auth);
-    app.use("/admin", admin);
-    app.use("/manager", manager);
-    app.use("/coordinator", coordinator);
-    io.on("connection", (socket) => {
-      console.log("Socket connected:", socket.id);
-    });
-    httpServer.listen(PORT, () => {
-      console.log(`Server is starting at http://${HOST}:${PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await prisma.$disconnect();
-  }
-};
+const wss = new WebSocket.Server({ server });
 
-main().catch(console.error);
+wss.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+app.use("/user", user);
+app.use("/auth", auth);
+app.use("/admin", admin);
+app.use("/manager", manager);
+app.use("/coordinator", coordinator);
+app.use("/chat", chat)
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Process terminated');
+    prisma.$disconnect();
+  });
+});
