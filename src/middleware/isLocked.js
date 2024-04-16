1359f0f153b11e6d8477cd4081df0ae763dc1cd0
minhimmaturity <trantanminh0603@gmail.com>
2024-04-16 22:25:30 +0700

    fix isLocked

--- src/middleware/isLocked.js
+++ src/middleware/isLocked.js
@@ -1,24 +1,29 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const isLocked = async (req, res, next) => {
   try {
-
-    const user = await prisma.user.findUnique({
-      where: { email: req.decodedPayload.data.email },
-
-    });
-    console.log("dấdsdasdasdas", user.email);
+    let user;
+    if (!req.decodedPayload) {
+      const { email } = req.body;
+      user = await prisma.user.findUnique({
+        where: { email: email },
+      });
+    } else {
+      user = await prisma.user.findUnique({
+        where: { email: req.decodedPayload.data.email },
+      });
+    }
 
     if (user.is_locked) {
-      return res.status(403).json({ error: 'Your account is locked.' });
+      return res.status(403).json({ error: "Your account is locked." });
     }
 
     next();
   } catch (error) {
-    console.error('Error checking if user is locked:', error);
-    return res.status(500).json({ error: 'Failed to check if user is locked' });
+    console.error("Error checking if user is locked:", error);
+    return res.status(500).json({ error: "Failed to check if user is locked" });
   }
-}
+};
 
 module.exports = isLocked;
