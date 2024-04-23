0da3dd7feda3a54aa9c1f6256134c6b5732d27cb
minhimmaturity <trantanminh0603@gmail.com>
2024-04-23 16:30:33 +0700

    update refresh access token

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -137,8 +137,13 @@ const refreshToken = async (token, res, req, next) => {
         refreshToken,
         process.env.REFRESH_SECRET_KEY
       );
+
+      const user = await prisma.user.findFirst({where: {email: refreshedUser.data.email}})
+      const name = user.name
+      const email = user.email
+      const role = user.role
       const newAccessToken = jwt.sign(
-        { data: refreshedUser.data },
+        { data: {name, email, role} },
         process.env.SECRET_KEY,
         {
           expiresIn: "3d",
